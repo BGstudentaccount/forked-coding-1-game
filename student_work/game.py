812bd1d63d62d52cfa40336be1361ca70789ee1c
micahw00 <s1000891@student.knoxschools.org>
2026-03-03 17:01:02 +0000

    added move player function

--- student_work/game.py
+++ student_work/game.py
@@ -59,6 +59,33 @@ def draw_board(stdscr):
                 row += game_data['empty']
         stdscr.addstr(y, 0, row, curses.color_pair(1))
 
+def move_player(key):
+    x = game_data['player']['x']
+    y = game_data['player']['y']
+
+    new_x, new_y = x, y
+    key = key.lower()
+
+    if key == "w" and y > 0:
+        new_y -= 1
+    elif key == "s" and y < game_data['height'] - 1:
+        new_y += 1
+    elif key == "a" and x > 0:
+        new_x -= 1
+    elif key == "d" and x < game_data['width'] - 1:
+        new_x += 1
+    else:
+        return  # Invalid key or move off board
+
+    # Check for obstacles
+    if any(o['x'] == new_x and o['y'] == new_y for o in game_data['obstacles']):
+        return
+
+    # Update position and increment score
+    game_data['player']['x'] = new_x
+    game_data['player']['y'] = new_y
+    game_data['player']['score'] += 1
+
     stdscr.refresh()
     stdscr.getkey()  # pause so player can see board
 
